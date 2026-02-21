3da385c1cad3a9047a44624c96d08d7c0dbceeab
Samrat <ksamrat224@gmail.com>
2026-02-21 20:22:11 +0545

    chore: add basic Express server setup with package.json, server.js, app.js, and .gitignore

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+const app = express(); // Create an instance of the Express application
+
+
+module.exports = app; // Export the app instance for use in other files
