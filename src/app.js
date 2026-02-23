78d717e9871e9d08914a6d9a2170ca3d57dcd977
Samrat <ksamrat224@gmail.com>
2026-02-23 21:44:46 +0545

    feat: add cookie-parser middleware and set auth token cookie

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser"); // Import cookie-parser middleware
 const authRouter = require("./routes/auth.routes"); // Import the authentication routes
 
 const app = express(); // Create an instance of the Express application
 
 app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cookieParser()); // Use cookie-parser middleware to parse cookies in requests
 
 app.use("/api/auth", authRouter); // Use the authentication routes for any requests to /api/auth
 
