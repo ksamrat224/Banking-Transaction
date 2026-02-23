d459091fbe7496c29ce7621ed4e1e7179b65aeff
Samrat <ksamrat224@gmail.com>
2026-02-23 21:13:58 +0545

    feat: add JWT token generation to user registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 /**
  * - user register controller
@@ -13,6 +14,9 @@ async function userRegisterController(req, res) {
       .json({ message: "User already exists", status: "fail" });
   }
   const user = await userModel.create({ name, email, password });
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 }
 
 module.exports = {
